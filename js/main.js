b5d4e808ea507c498afcf21f242023a014b4c096
Jaakko Honkanen <javiho@utu.fi>
2019-01-02 01:17:10 +0200

    refactored

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// TODO: jaa tämä useaan tiedostoon tai muuten selkeytä
+
 let width = 960,
     height = 500;
 const defaultRotationLongitude = -180;
@@ -35,7 +37,7 @@ let tokenData = [];
 let selectedTokens = [];
 const centroidFill = "rgba(255, 128, 0, 1)";
 
-initialize();
+initializeDocument();
 
 // load and display the World
 d3.json("world-110m.json", function(error, topology) {
@@ -55,8 +57,8 @@ d3.json("world-110m.json", function(error, topology) {
         .attr("d", path)
         .attr("fill", "black")
         .attr("stroke", "red")
-        .attr("stroke-width", "1")
-        .on("click", handleCountryClick);
+        .attr("stroke-width", "1");
+        //.on("click", handleCountryClick); This is replaced with universal click handler.
 
     // TODO: mihin tätä käytetään?
     //var countries = topojson.feature(topology, topology.objects.countries).features;
@@ -65,8 +67,8 @@ d3.json("world-110m.json", function(error, topology) {
         countryNames = data;
         console.log("country names:", countryNames);
         initializeCountryData(pathDataArray);
-        updateCentroidCircles2();
-        updateToppingCircles2();
+        updateCentroidCircles();
+        updateToppingCircles();
     });
 });
 
@@ -87,8 +89,8 @@ var zoom = d3.behavior.zoom()
         // FYI: d3.event.translate on zoom-objectin arvo.
         //console.log("translation1:", d3.event.translate, "scale:", d3.event.scale);
 
-        updateToppingCircles2();
-        updateCentroidCircles2();
+        updateToppingCircles();
+        updateCentroidCircles();
 
         // path.projection palauttaa funktion. Joten miten sen voi asettaa HTML-elementin
         // attribuutin arvoksi? - Funktiota ei asetettane suoraan arvoksi, vaan d3:ssa
@@ -103,7 +105,7 @@ var zoom = d3.behavior.zoom()
 
 svg.call(zoom);
 
-/*
+/* TODO: jos halutaan piirrellä viivoja
 g.append("line")
     .attr("x1", "0")
     .attr("y1", "0")
@@ -112,6 +114,21 @@ g.append("line")
     .attr("stroke", "rgb(255,0,0)")
     .attr("stroke-width", "2");
     //.style("stroke:rgb(255,0,0);stroke-width:2");*/
+
+function initializeDocument(){
+    document.addEventListener("click", universalClickHandler);
+    /* TODO: jos halutaan tunnistaa muiden kuin shift ym. helposti tunnistettavia näppäimien pohjassapito.
+    document.addEventListener("keyup", function(event){
+        console.log("key up:", event.key);
+    });
+    document.addEventListener("keydown", function(event){
+        console.log("key down:", event.key, event.key === "a");
+    });*/
+}
+
+
+
+/*************************** Data for rendering ******************************/
 
 function initializeCountryData(topology){
     centroidData = topology
@@ -159,7 +176,54 @@ function updateTokenData(){
     tokenData = newTokenData;
 }
 
-function updateCentroidCircles2(){
+function getTextElementData(){
+    const textElementData = [];
+    countryData.forEach(function(countryPresentation){
+        const tokensInCountry = tokenService.getTokensInCountry(countryPresentation.id);
+        if(tokensInCountry.length > 0){
+            const newTextElementEntry = {
+                lon: countryPresentation.centroid[0],
+                lat: countryPresentation.centroid[1],
+                amountOfTokens: tokensInCountry.length,
+                isLabel: true
+            };
+            textElementData.push(newTextElementEntry);
+        }
+    });
+    return textElementData;
+}
+
+/*
+    Gets country entry from countryData by country id, or undefined if it doesn't exist.
+ */
+function getCountryEntryById(countryId){
+    const countryEntry = countryData.find(function(element){
+        return element.id === countryId;
+    });
+    return countryEntry;
+}
+
+/*
+    Pre-condition: there is a country name for idString in countryNames.
+ */
+function getCountryNameById(idNumber){
+    //const idNumber = parseInt(idString);
+    const countryEntry = countryNames.find( entry => parseInt(entry.id) === idNumber );
+    if(countryEntry === undefined){
+        console.log("getCountryNameById: idNumber:", idNumber);
+    }
+    const name = countryEntry.name;
+    console.assert(name !== undefined);
+    return name;
+}
+
+/*************************** Data for rendering ends ******************************/
+
+
+
+/****************************Update functions *************************************/
+
+function updateCentroidCircles(){
     const extendedCentroidData = centroidData.map(function(element){
         const centroidEntry = {centroid: element};
         centroidEntry.isCentroid = true;
@@ -184,7 +248,7 @@ function updateCentroidCircles2(){
     drawInCorrectOrder();
 }
 
-function updateToppingCircles2(){
+function updateToppingCircles(){
     updateTokenData();
     let selection = g.selectAll(".topping-circle").data(tokenData);
     selection.enter()
@@ -227,32 +291,8 @@ function updateToppingCircles2(){
 }
 
 function updateTokenStackNumbers(){
-    /*
-    käy läpi kaikki alueet
-        jos on tookkeni alueella (1 tai useampi), pistä päälle numero
-        jos ei ole, ota mahdollinen numero pois
-
-    laabeleilla pitäisi olla koordinaatti
-
-    kun entteröidään, appendaa
-    kaikilla on datassa pituus ja leveys ja koordinaatit lasketaan aina projektiolla
-    kun exitoidaan, remoovataan
-    */
-    const textElementData = [];
-    countryData.forEach(function(countryPresentation){
-        const tokensInCountry = tokenService.getTokensInCountry(countryPresentation.id);
-        if(tokensInCountry.length > 0){
-            const newTextElementEntry = {
-                lon: countryPresentation.centroid[0],
-                lat: countryPresentation.centroid[1],
-                amountOfTokens: tokensInCountry.length,
-                isLabel: true
-            };
-            textElementData.push(newTextElementEntry);
-        }
-    });
+    const textElementData = getTextElementData();
     //console.log("textElementData:", textElementData);
-
     let selection = g.selectAll(".token-counter-number").data(textElementData);
     selection
         .enter()
@@ -315,22 +355,59 @@ function getElementPriority(elementData){
     return background;
 }
 
-function handleCountryClick(d, i){
-    const countryName = getCountryNameById(d.id);
-    console.log("clicked:", countryName, "id:", d.id);
-    //alert(countryName + "Click'd!");
-    //console.log("datum:", d);
-    //console.log("index:", i);
-}
+/**************************** Update functions ends *************************************/
 
-/*
-    Gets country entry from countryData by country id, or undefined if it doesn't exist.
- */
-function getCountryEntryById(countryId){
-    const countryEntry = countryData.find(function(element){
-        return element.id === countryId;
-    });
-    return countryEntry;
+
+
+/**************************** User actions *************************************/
+
+function universalClickHandler(event){
+    const target = event.target;
+    console.log("event target:");
+    console.log(target);
+    const targetD3 = d3.select(target);
+    const datum = targetD3.datum();
+    if(datum !== undefined) {
+        if(datum.isCountry) {
+            const countryId = datum.id;
+            if(!event.shiftKey) {
+                const countryName = getCountryNameById(countryId);
+                //console.log("datum:", datum);
+                //console.log("countryName:", countryName);
+                addToken(countryId);
+            }else{
+                const countryEntry = countryData.find(x => x.id === countryId);
+                console.assert(countryEntry !== undefined);
+                const centroid = countryEntry.centroid;
+                //console.log("country entry:", countryEntry);
+                const selectedTokens3Dselection = d3.selectAll('.topping-circle').filter(function(d){
+                    for(let selectedTokenDatum of selectedTokens){
+                        return d.token.id === selectedTokenDatum.token.id;
+                    }
+                });
+                // Update model before rendering transition.
+                for(let selectedTokenDatum of selectedTokens){
+                    //console.log("selectedTokenDatum", selectedTokenDatum);
+                    tokenService.moveToken(selectedTokenDatum.token.id, countryEntry.id);
+                }
+                console.log("selectedTokens3Dselection", selectedTokens3Dselection);
+                transitionTokens(selectedTokens3Dselection, centroid);
+                // TODO: jostakin syystä jos tässä kutsutaan updateToppingCircles, se keskeyttää transition,
+                // mutta jos sitä kutsutaan muuten transition aikana, se ei keskeytä sitä. Miksi?
+            }
+        }
+        else if(datum.isToken){
+            const tokenId = datum.token.id;
+            console.assert(tokenId !== undefined);
+            console.log("token clicked:", tokenId);
+            selectedTokens = [];
+            if(!selectedTokens.find(x => datum.token.id === x.token.id)){
+                selectedTokens.push(datum);
+                console.log("tokenD3", targetD3.datum());
+                updateToppingCircles();
+            }
+        }
+    }
 }
 
 /*
@@ -344,122 +421,43 @@ function addToken(countryId){
     }
     tokenService.addToken(countryEntry.id);
     console.log("all tokens:", tokenService.tokens);
-    updateToppingCircles2();
+    updateToppingCircles();
 }
 
-function calculateNewStrokeWidth(scale, normalWidth){
-    return (normalWidth / scale).toString();
+function transitionTokens(selectedTokens3Dselection, targetGeographicCoordinates){
+    selectedTokens3Dselection.transition()
+        .each("end", function(){
+            updateToppingCircles();
+        })
+        .duration(2000)
+        .attr("cx", function(d) {
+            return projection([targetGeographicCoordinates[0], targetGeographicCoordinates[1]])[0];
+        })
+        .attr("cy", function(d) {
+            return projection([targetGeographicCoordinates[0], targetGeographicCoordinates[1]])[1];
+        });
+    drawInCorrectOrder();
 }
+
+
+/**************************** User actions ends *************************************/
+
+
+
+/**************************** Other *************************************/
+
+/* TODO: jos halutaan käyttää d3:n .on() metodia maan kuuntelemiseen
+function handleCountryClick(d, i){
+    const countryName = getCountryNameById(d.id);
+    console.log("clicked:", countryName, "id:", d.id);
+    //alert(countryName + "Click'd!");
+    //console.log("datum:", d);
+    //console.log("index:", i);
+}*/
 
 /*
-    Pre-condition: there is a country name for idString in countryNames.
+    TODO: Poissa käytöstä.
  */
-function getCountryNameById(idNumber){
-    //const idNumber = parseInt(idString);
-    const countryEntry = countryNames.find( entry => parseInt(entry.id) === idNumber );
-    if(countryEntry === undefined){
-        console.log("getCountryNameById: idNumber:", idNumber);
-    }
-    const name = countryEntry.name;
-    console.assert(name !== undefined);
-    return name;
-}
-
-function addCircleToArea(pathD3){
-    const newCircleCenter = pathD3.centroid()
-}
-
-function initialize(){
-    document.addEventListener("click", universalClickHandler);
-    /*document.addEventListener("keyup", function(event){
-        console.log("key up:", event.key);
-    });
-    document.addEventListener("keydown", function(event){
-        console.log("key down:", event.key, event.key === "a");
-    });*/
-}
-
-function universalClickHandler(event){
-    const target = event.target;
-    console.log("event target:");
-    console.log(target);
-    const targetD3 = d3.select(target);
-    const datum = targetD3.datum();
-    if(datum !== undefined) {
-        if(datum.isCountry) {
-            const countryId = datum.id;
-            if(!event.shiftKey) {
-                const countryName = getCountryNameById(countryId);
-                console.log("datum:", datum);
-                console.log("countryName:", countryName);
-                addToken(countryId);
-            }else{
-                // TODO: tähän jäätiin!
-                /*
-                tiedetään valittyjen tookkeneiden daattumit
-                tiedetään mihin pitäisi liikuttaa, edellisessä blokissa
-                jotenkin animoidaan se, että liikutetaan sen maan sentroidiin,
-                mutta ei renderöidä vielä uudestaan tookkeneita, koska muutenhan
-                tokeni näyttäytyisi uudessa paikassa heti. (Tai voitaisiin animoida joku toinen
-                objekti, esim vauhtiviivat tai haamu? Ideaalista olisi jos olisi sama klikattava ym
-                tookkeni joka on animoitavana).
-                jos käytetään animointiin samaa objektia, animoinnin jälkeen renderöidään uudestaan
-                tookkenit.
-
-                // TODO: olisi hyvä olla sekä mahdollisuus olla uudestaanrenderöimättä tokenia
-                kun transitio on kesken (tunnnistamienn renderöintimetodissa) sekä renderöidä uudestaan
-                kun transitio on päättynyt kyseisellä tokenilla.
-                 */
-                //console.log("pitäisi liikuyttaa tookkenia");
-                const countryEntry = countryData.find(x => x.id === countryId);
-                console.assert(countryEntry !== undefined);
-                const centroid = countryEntry.centroid;
-                //console.log("country entry:", countryEntry);
-                const selectedTokens3Dselection = d3.selectAll('.topping-circle').filter(function(d){
-                    //console.log("topping cicgle d:", d);
-                    //console.log("selectedTokens in here", selectedTokens);
-                    for(let selectedTokenDatum of selectedTokens){
-                        //console.log("selectedTokenDatum", selectedTokenDatum);
-                        return d.token.id === selectedTokenDatum.token.id;
-                    }
-                });
-                // Update model before rendering transition.
-                for(let selectedTokenDatum of selectedTokens){
-                    //console.log("selectedTokenDatum", selectedTokenDatum);
-                    tokenService.moveToken(selectedTokenDatum.token.id, countryEntry.id);
-                }
-                console.log("selectedTokens3Dselection", selectedTokens3Dselection);
-                selectedTokens3Dselection.transition()
-                    .each("end", function(){
-                        updateToppingCircles2();
-                    })
-                    .duration(2000)
-                    .attr("cx", function(d) {
-                        //console.log("d:", d);
-                        return projection([centroid[0], centroid[1]])[0];
-                    })
-                    .attr("cy", function(d) {
-                        return projection([centroid[0], centroid[1]])[1];
-                    });
-                // TODO: jostakin syystä jos tässä kutsutaan updateToppingCircles2, se keskeyttää transition,
-                // mutta jos sitä kutsutaan muuten transition aikana, se ei keskeytä sitä. Miksi?
-                drawInCorrectOrder();
-            }
-        }
-        else if(datum.isToken){
-            const tokenId = datum.token.id;
-            console.assert(tokenId !== undefined);
-            console.log("token clicked:", tokenId);
-            //const tokenD3 = d3.select('[data-token-id='+tokenId+"]");
-            //const oldData = targetD3.data();
-            //const newData = oldData.selected = true;
-            //targetD3.data(newData);
-            selectedTokens = [];
-            if(!selectedTokens.find(x => datum.token.id === x.token.id)){
-                selectedTokens.push(datum);
-                console.log("tokenD3", targetD3.datum());
-                updateToppingCircles2();
-            }
-        }
-    }
+function calculateNewStrokeWidth(scale, normalWidth){
+    return (normalWidth / scale).toString();
 }
